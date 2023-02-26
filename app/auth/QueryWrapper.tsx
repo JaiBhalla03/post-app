fe2e9421db18856a4bbef5a222b4e4ed942a408f
jai <jai.bhalla2021@vitstudent.ac.in>
2023-02-26 11:59:02 +0530

    correct the errors

--- app/auth/QueryWrapper.tsx
+++ app/auth/QueryWrapper.tsx
@@ -2,6 +2,7 @@
 
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {Toaster} from "react-hot-toast";
+import {ReactNode} from "react";
 
 const queryClient = new QueryClient()
 
